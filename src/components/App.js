5d7c2aad832562a18401593abdd0670510fc8dae
zdrale <nikolazd22@outlook.com>
2021-09-05 18:00:29 +0200

    Added api link and params to the axios get method

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
+import axios from 'axios';
+
 import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.linkpreview.net', {
+            params: {key: '47caa4074eb0734eb2a59414cc3a1639',q: term}
+        });
     }
 
     render() {
