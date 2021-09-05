123ad4507a1e66dc2ebf246cfce58bd4160049d1
zdrale <nikolazd22@outlook.com>
2021-09-05 19:24:42 +0200

    Made onSearchSubmit async

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,11 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        axios.get('https://api.linkpreview.net', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.linkpreview.net', {
             params: {key: '47caa4074eb0734eb2a59414cc3a1639',q: term}
         });
+        console.log(response.data);
     }
 
     render() {
