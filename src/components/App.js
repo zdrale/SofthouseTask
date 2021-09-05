cfb0bd12272491132c67a76614d0be0fa92c3fed
zdrale <nikolazd22@outlook.com>
2021-09-05 21:44:08 +0200

    Refactored onSearchSubmit with arrow functions

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,21 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    async onSearchSubmit(term) {
+    state = { website: [] };
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.linkpreview.net', {
             params: {key: '47caa4074eb0734eb2a59414cc3a1639',q: term}
         });
-        console.log(response.data);
+        console.log(response);
+        this.setState({website: response.data})
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.website.description} 
             </div>
         );
     }
